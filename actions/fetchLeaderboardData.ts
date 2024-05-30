8f66927e12beaab1d9ca5aca8a029e03fab69d6d
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-30 23:26:28 +0300

    make

--- actions/fetchLeaderboardData.ts
+++ actions/fetchLeaderboardData.ts
@@ -1,21 +1,44 @@
 'use server';
 
-import { Analitycs } from "@/app/[locale]/leaderboard/page";
 
 
-
-
-  const LEADERBOARD_ROUTE =
-    'https://baas-data-provider.onrender.com/leaderboard';
+  // const LEADERBOARD_ROUTE =
+    // 'https://baas-data-provider.onrender.com/leaderboard';
   const LEADERBOARD_ROUTE_LOCAL = 'http://localhost:8080/leaderboard'; // If you want to use local server use
 
+export type Analytics = {
+  members: {
+    name: string;
+    node_id: string;
+    projects_names: {
+      url: string;
+      name: string;
+    }[];
+    avatar_url: string;
+    score: number;
+    stats: {
+      additions: number;
+      deletions: number;
+      commits: number;
+    };
+  }[];
+  since: number;
+  until: number;
+  stat: 'allTimes' | 'lastMonth' | 'lastWeek';
+};
+
+
+
 async function fetchLeaderboard() {
-  const response = await fetch(LEADERBOARD_ROUTE)
+  const response = await fetch(LEADERBOARD_ROUTE_LOCAL)
 
   // fetch from endpoint POST with page, limit, filter as ILeaderboardResponse
-  const data = await response.json() as unknown as Analitycs;
+  const data = await response.json() as unknown as [Analytics, Analytics, Analytics];
 
   return data;
 }
 
 export default fetchLeaderboard;
+
+
+
--- app/[locale]/leaderboard/page.tsx
+++ app/[locale]/leaderboard/page.tsx
@@ -1,26 +1,5 @@
 import Leaderboard from '@/components/Leaderboard/Leaderboard';
 
-export type Analitycs = {
-  members: {
-    name: string;
-    node_id: string;
-    projects_names: {
-      url: string;
-      name: string;
-    }[];
-    avatar_url: string;
-    score: number;
-    stats: {
-      additions: number;
-      deletions: number;
-      commits: number;
-    };
-  }[];
-  since: string;
-  until: string;
-};
-
-
 const LeaderboardPage: React.FC = () => {
   return (
     <>
